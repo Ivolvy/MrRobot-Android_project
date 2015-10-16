c1fb264a707e4a9f429ae301cd002747ac0677a1
Ivolvy <michael.genty@free.fr>
2015-10-16 11:38:15 +0200

    Added splashScreen

---
+++ app/src/main/java/com/example/mgenty/mrrobot_android_project/splashscreen/SplashScreenActivity.java
@@ -0,0 +1,35 @@
+package com.example.mgenty.mrrobot_android_project.splashscreen;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mgenty.mrrobot_android_project.HomeActivity;
+import com.example.mgenty.mrrobot_android_project.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreenActivity.this, HomeActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
